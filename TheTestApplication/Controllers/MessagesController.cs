70803f6a6ba00dc874166b329a6ac045efc6eb8d
betoz <alberto.arango.munoz@outlook.com>
2020-01-31 16:19:10 +0100

    Replace weather with messages API

---
+++ TheTestApplication/Controllers/MessagesController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TheTestApplication.Models;
+
+namespace TheTestApplication.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class MessagesController : ControllerBase
+    {
+        private static readonly MessageLog[] Messages = new MessageLog[]
+        {
+            new MessageLog{Id=1, Message="Hello world"},
+            new MessageLog{Id=2, Message="Come here"},
+            new MessageLog{Id=3, Message="Pasta for lunch"},
+            new MessageLog{Id=4, Message="Welcome home"},
+            new MessageLog{Id=5, Message="There is more"}
+        };
+
+        [HttpGet]
+        public MessageLog[] GetMessages()
+        {
+            return Messages;
+        }
+    }
+}
---
+++ TheTestApplication/Models/MessageLog.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace TheTestApplication.Models
+{
+    public class MessageLog
+    {
+        public long Id { get; set; }
+        public string Message { get; set; }
+    }
+}
