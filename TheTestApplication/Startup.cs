89487c3da7ab6f3e84f1a1ca2cd5f443ee1a9a24
betoz <alberto.arango.munoz@outlook.com>
2020-01-31 13:54:40 +0100

    Remove stuff not needed

--- TheTestApplication/Startup.cs
+++ TheTestApplication/Startup.cs
@@ -1,15 +1,8 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-
 namespace TheTestApplication
 {
     public class Startup
@@ -36,8 +29,6 @@ namespace TheTestApplication
             }
 
             app.UseRouting();
-
-            app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
